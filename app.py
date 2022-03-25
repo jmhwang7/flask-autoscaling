a26ed5c719e32d03105f18962e95f6bff7e68660
Jennifer Hwang <jennifer@render.com>
2022-03-24 18:20:33 -0700

    Fix things

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 import aioredis
-redis = aioredis.from_url("redis://red-c8sf3jd0maldij14587g:6379")
+import asyncio
+
+async def main():
+    redis = aioredis.from_url("redis://red-c8sf3jd0maldij14587g:6379")
+    await redis.set("my-key", "value")
+    value = await redis.get("my-key")
+    print(value)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
