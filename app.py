d0569d049e027ea4ccc01a072717144afdc581b5
Jennifer Hwang <jennifer@render.com>
2022-03-24 17:07:03 -0700

    simplify

--- app.py
+++ app.py
@@ -1,8 +1,2 @@
-from flask import Flask
 import aioredis
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    redis = aioredis.from_url("redis://red-c8sf3jd0maldij14587g:6379")
-    return 'Hello, World!'
+redis = aioredis.from_url("redis://red-c8sf3jd0maldij14587g:6379")
